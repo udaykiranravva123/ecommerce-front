7ce557bb0f07a26c6d0c4a84033e7f6fd6d28331
uday <udaykiranravva123@gmail.com>
2020-06-25 07:02:39 -0700

    push to origin

---
+++ src/user/Signin.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import Layout from "../core/Layout";
+import { signin, authenticate, isAuthenticated } from "../auth";
+import SocialLogin from "./SocialLogin";
+
+const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    redirectToReferrer: false
+  });
+
+  const { email, password, loading, error, redirectToReferrer } = values;
+  const { user } = isAuthenticated();
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        authenticate(data, () => {
+          setValues({
+            ...values,
+            redirectToReferrer: true
+          });
+        });
+      }
+    });
+  };
+
+  const signUpForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input
+          onChange={handleChange("email")}
+          required
+          type='email'
+          className='form-control'
+          value={email}
+          required
+        />
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input
+          onChange={handleChange("password")}
+          type='password'
+          className='form-control'
+          value={password}
+          required
+        />
+      </div>
+      <button onClick={clickSubmit} className='btn btn-primary'>
+        Submit
+      </button>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className='alert alert-danger'
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showLoading = () =>
+    loading && (
+      <div className='alert alert-info'>
+        <h2>Loading...</h2>
+      </div>
+    );
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      if (user && user.role === 1) {
+        return <Redirect to='/admin/dashboard' />;
+      } else {
+        return <Redirect to='/user/dashboard' />;
+      }
+    }
+    if (isAuthenticated()) {
+      return <Redirect to='/' />;
+    }
+  };
+
+  return (
+    <Layout
+      title='Signin'
+      description='Signin to Node React E-commerce App'
+      className='container col-md-8 offset-md-2'
+    >
+      {showLoading()}
+      {showError()}
+      {signUpForm()}
+      {redirectUser()}
+      <br />
+      <br />
+      <Link to='/forgot-password' className='btn btn-raised btn-danger'>
+        {" "}
+        Forgot Password
+      </Link>
+    </Layout>
+  );
+};
+
+export default Signin;
